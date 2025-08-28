6642afdd097fd004daafb93fffae32bdb9d1025a
Daniel Muhia <muhiadan17@gmail.com>
2025-08-28 14:38:15 +0300

    adds choices to main menu to enable navigation

--- cli.py
+++ cli.py
@@ -16,4 +16,15 @@ class CLI:
 
     while True:
       display_main_menu()
-      
+      choice = input("> ")
+      if choice == "1":
+        self.handle_scrub_files()
+      elif choice == "2":
+        self.handle_manage_profiles()
+      elif choice == "3":
+        self.handle_view_audit_trail()
+      elif choice == "4":
+        console.print("[bold]Goodbye![/bold]")
+        sys.exit()
+      else:
+        console.print("[bold red]Invalid choice. Please try again.[/bold red]")
